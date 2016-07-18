183287b55d1e2bb29736057f17f9a0cd024fe15e
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-07-17 22:27:32 -0400

    files are linked, but game is looping like crazy

--- main.js
+++ main.js
@@ -1,61 +1,44 @@
 //File to contain the logic of your app. Running it in Terminal/Bash will start the game.
+var word = require('./word.js');
+var inquirer = require('inquirer');
+
 //VARIABLES
-var guessCounter = 0;
 var remainingGuesses = 10;
 var lettersGuessed = [];
-var gameOver = false;
-var found = false;
-
-
-//Pick a random word (array for now, constructor later)
-var words = ["dory", "nemo", "marlin", "becky", "gerald", "hank", "destiny", "squirt", "crush", "bailey", "rudder", "fluke"];
-
-var word = words[Math.floor(Math.random() * words.length)];
+var answerArray = [];
 var string;
-//display word spaces
-var answerArray =[];
-  for (var i = 0; i<word.length; i++){
-    answerArray[i] = "_";
-  }
 
+
+var playGame = function(){
 
 while (remainingGuesses > 0 && word !==string ){
+
+
+//Get guess from player
+
+console.log("Can you guess who is swimming nearby?  Guess their name to find them!")
+
+    //RUNS inquirer AND ASKS THE USER TO GUESS A LETTER
+    inquirer.prompt([{
+      name: "guess",
+      type: "input",
+      message: "Guess a letter!"
+
+    }]).then(function(answers){
+      //var newGuess = new Word
+      console.log(answers)
+    });
+    remainingGuesses--;
 console.log(remainingGuesses);
 console.log(word);  
-console.log(answerArray.join(" "));
+//console.log(LetterSpaces.answerArray.join(" "));
+
 var string = answerArray.join('');
     console.log(string);
  if (word == string) {
     console.log("congratulations! You caught " + word + "!");
           }
-//Get guess from player (prompt for now, inquirer later)
-var guess = prompt("Guess a letter!");
-//check if guess is a valid letter
-  if (guess.length !== 1){
-    alert ("Please enter a single letter");
-  } else {
-
-    
-    //check if letter is in secret word
-      for (var j = 0; j < word.length; j++){
-        if(word[j] === guess) {
-          answerArray[j] = guess;
-                
-      } 
   }
-      
+}  
+  
 
-
-    
-  }
-
-  //keep track of letters that have been guessed
-
-//update the word display
-
-//update the letters remaining
-
-//display winning message if player guesses the word
-
-//display start over message if player loses
-}
